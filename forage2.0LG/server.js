06c06a6a0f09bef40ceee9fca583e133eeae8dbe
Kappisan <wilkosz.ian@gmail.com>
2016-10-30 17:14:32 +0000

    remove categories from server

--- forage2.0LG/server.js
+++ forage2.0LG/server.js
@@ -18,76 +18,17 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 var recipes = JSON.parse(fs.readFileSync('savedRecepies.json', 'utf8'));
 
+// give all the recepies unique ID's
 recipes.forEach(function(d,i) {
 	d.id = i;
 });
-
-
-
-var categories = [
-{ "name": "Vegetables", cookierate: 0 },
-{ "name": "Eggs", cookierate: 0 },
-{ "name": "Chicken", cookierate: 0 },
-{ "name": "Pasta", cookierate: 0 },
-{ "name": "Fish", cookierate: 0 },
-{ "name": "Bread", cookierate: 0 },
-{ "name": "Lamb", cookierate: 1 },
-{ "name": "Fruit", cookierate: 0 },
-{ "name": "Beef", cookierate: 1 },
-{ "name": "Cheese", cookierate: 0 },
-{ "name": "Pork", cookierate: 1 },
-{ "name": "Rice", cookierate: 0 },
-{ "name": "Turkey", cookierate: 0 },
-{ "name": "Goose", cookierate: 0 },
-{ "name": "Seafood", cookierate: 0 },
-{ "name": "Chocolate", cookierate: 0 },
-{ "name": "Game", cookierate: 0 },
-{ "name": "Duck", cookierate: 0 },
-{ "name": "Avocado", cookierate: 0 },
-{ "name": "Sausage", cookierate: 0 },
-{ "name": "Salmon", cookierate: 0 },
-{ "name": "Quinoa", cookierate: 0 },
-{ "name": "Courgette", cookierate: 0 },
-{ "name": "Aubergine", cookierate: 0 },
-{ "name": "Couscous", cookierate: 0 },
-{ "name": "Mince", cookierate: 0 },
-{ "name": "Kale", cookierate: 0 },
-{ "name": "Potato", cookierate: 0 },
-{ "name": "Spinach", cookierate: 0 },
-{ "name": "Lentil", cookierate: 0 },
-{ "name": "Rhubarb", cookierate: 0 },
-{ "name": "Scallops", cookierate: 0 },
-{ "name": "Halloumi", cookierate: 0 },
-{ "name": "Steak", cookierate: 0 },
-{ "name": "Tofu", cookierate: 0 },
-{ "name": "Mackerel", cookierate: 0 },
-{ "name": "Mushroom", cookierate: 0 },
-{ "name": "Beetroot", cookierate: 0 },
-{ "name": "Leek", cookierate: 0 },
-{ "name": "Cauliflower", cookierate: 0 },
-{ "name": "Chorizo", cookierate: 0 },
-{ "name": "Pumpkin", cookierate: 0 },
-{ "name": "Asparagus", cookierate: 0 },
-{ "name": "Broccoli", cookierate: 0 },
-{ "name": "Tuna", cookierate: 0 },
-{ "name": "Gammon", cookierate: 0 },
-{ "name": "Apple", cookierate: 0 },
-{ "name": "Seabass", cookierate: 0 },
-];
 
 
 var foods = [
 	{ "name": "Apple" },
 	{ "name": "Breads" },
 	{ "name": "orange" }
-]
-
-
-
-app.get('/api/categories', function (req, res) {
-
-	res.send(categories);
-});
+];
 
 
 app.get('/api/foods', function (req, res) {
@@ -135,11 +76,7 @@ app.post('/api/add', function (req, res) {
 
 app.get('/api/recipe', function (req, res) {
 
-
 	console.log('req.query', req.query);
-
-	
-
 	res.send([recipes[req.query.id]]);
 });
 
@@ -150,4 +87,4 @@ server = app.listen(PORT);
 var host = server.address().address
 var port = server.address().port
 
-console.log("muncher rest server listening on port %s", port);
+console.log("forage server listening on port %s", port);
