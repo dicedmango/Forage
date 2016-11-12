b4314e392e1d3dca2aa326c313e3f305403a96d1
Kappisan <wilkosz.ian@gmail.com>
2016-11-12 23:32:25 +0000

    dynamic did you know

--- angular/script.js
+++ angular/script.js
@@ -124,13 +124,78 @@ var app = angular.module('app', ['ngRoute', 'ngAnimate'])
 		.when('/about', { templateUrl: 'templates/about.html' })
 		.when('/search', { templateUrl: 'templates/search.html', controller: 'SearchCtrl' })
 		.when('/cookies', { templateUrl: 'templates/cookies.html', controller: 'CookieCtrl' })
-		.when('/duk', { templateUrl: 'templates/duk.html' })
+		.when('/duk', { templateUrl: 'templates/duk.html', controller: 'DUKCtrl' })
 		.when('/user', { templateUrl: 'templates/user.html', controller: 'UserCtrl' })
 		.otherwise({ redirectTo: '/' });
 }]);
 
 
 // CONTROLLERS
+
+app.controller('DUKCtrl', ['$scope', '$http', '$location', function($s, $http, $location) {
+
+	$s.pageClass = 'duk-page';
+	$s.duks = [
+		{ 
+			top: "26% of all vegetables grown are destroyed before they even reach the shops.",
+			bottom: "Embrace wonky fruit and vegetables, they still have as many nutrience and are just as tasty as perfectly shaped produce even though they are ugly. 6 Billion Pounds Of Perfectly Edible Produce Is Wasted Every Year, Simply Because It's Ugly."
+		}
+		,
+		{ 
+			top: "Write it down and you are much more likely to stick to it!",
+			bottom: "Write a shopping list by checking your cupboards, fridge and freezer before you set off. This will help you know what you need and help you avoid from purchasing unnecessary products. Hopefully this will help you dismiss those seductive deals presented by the supermarkets and keeping you within your budget."
+		}
+		,
+		{ 
+			top: "The Difference Between Best Before & Used By",
+			bottom: "Use by date is about safety and the most important date to remember! Foods can be eaten (and most can be frozen) up until the use by date, but not after. Best before date is about quality Best before date is about quality and not safety. The food will be safe to eat after this date but may not be at its best."
+		}
+		,
+		{ 
+			top: "1/3 of the food around the world dose not get eaten?",
+			bottom: "Be more open minded about how you buy and eat food. Try more unusual cuts of meat or buy sustainable whole fish and use the leftover bones and head to make a delicious stock."
+		}
+		,
+		{ 
+			top: "The Uk food industry wastes 10 million tones of food each year",
+			bottom: "Waste research company; WRAP estimates that 270,000 tonnes of discarded food could be suitable for redistribution. Currently only 47 tonnes of waste food finds its way to charity."
+		}
+		,
+		{ 
+			top: "Look Out For Pulled Eggs",
+			bottom: "These small eggs from young hens are not large enough to meet supermarkets criteria, so they are often wasted. In truth, they are the best quality egg you can buy, so keep an eye out for them on sites, such as themacsfarm.co.uk"
+		},
+		{ 
+			top: "American wastes 40% of their food amounting to $165 Billion Dollars straight in the trash",
+			bottom: "As food waste figures pile up, so do the number of people in the UK relying on Food Banks. 1,109,309 Three day emergency food supply packs are being given out over 2016."
+		},
+		{ 
+			top: "Portion Out Your Food!",
+			bottom: "Weigh out foods such as rice, pasta and vegetables so you don't end up cooking too much and throwing it away."
+		},
+		{ 
+			top: "Embrace Leftovers",
+			bottom: "Portion out leftovers into freezer bags, label and date them, then pop them in the freezer ready for a quick weekday supper."
+		},
+		{ 
+			top: "The average family in Britain throws away £680 worth of food per year.",
+			bottom: "32% of bread in the UK. gets thrown away, reuse stale bread. You can ransform stale bread, just sprinkle with water, then pop briefly in a warm oven. You can also make plenty of recipies using stale bread or feed it to the birds."
+		},
+		{ 
+			top: "Grow your own food!",
+			bottom: "Growing fruits and vegetables seems overwhelming to most of us, but it is much simpler than it sounds. Plus you do not have to trade in your urban lifestyle for a life in the sticks for self sufficiency. All you need is a few square feet outside, a water source, and a little time."
+		},
+		{ 
+			top: "French Law Forbids Food Waste By Supermarkets",
+			bottom: "France has became the first country in the world to ban supermarkets from throwing away or destroying food, forcing them to donate instead. Anti poverty protesters helped bring this law forward. The fact that France has done this means that there is no reason any other country should not enforce this law."
+		}
+	];
+
+	$s.duk = $s.duks[Math.floor(Math.random() * $s.duks.length)];
+
+	console.log("$s.duks", $s.duks);
+
+}]);
 
 app.controller('UserCtrl', ['$scope', '$http', '$location', function($s, $http, $location) {
 
